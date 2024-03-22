dc078871d7f877dc937a90c4a84decdd894c762c
Max <ki6syd@gmail.com>
2024-03-21 23:29:56 -0700

    si5351 configured

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -1,8 +1,9 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <si5351.h>
 #include "wifi_conn.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Si5351 si5351;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,12 +15,29 @@ void setup() {
 
   wifi_init();
 
+  Wire.begin(CLOCK_SDA, CLOCK_SCL);
+  Serial.print("[SI5351] Status: ");
+  Serial.println(si5351.si5351_read(SI5351_DEVICE_STATUS));
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF , 26000000 , 0);
+  
+  si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLA);
+  si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLB);
+
+  si5351.drive_strength(SI5351_CLK0, SI5351_DRIVE_2MA);
+  si5351.drive_strength(SI5351_CLK1, SI5351_DRIVE_2MA);
+  
+  si5351.set_freq(10000000 * 100, SI5351_CLK0);
+  si5351.set_freq(20000000 * 100, SI5351_CLK1);
+  si5351.set_freq(30000000 * 100, SI5351_CLK2);
+
+  si5351.output_enable(SI5351_CLK0, 1);
+  si5351.output_enable(SI5351_CLK1, 1);
+  si5351.output_enable(SI5351_CLK2, 0);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  Serial.println("test");
 
   digitalWrite(LED_GRN, HIGH);
   // wait for a second
