4136e4de0c893539c6d81ef4f06ca282ce6c867c
Max <ki6syd@gmail.com>
2024-03-31 21:49:35 -0700

    two mono sine waves (diff freq) to stereo out
    one frequency on LOUT1/LOUT2, other frequency on ROUT1/ROUT2

--- fw/src/main.cpp
+++ fw/src/main.cpp
@@ -11,33 +11,37 @@
 Si5351 si5351;
 // TwoWire clockBus = TwoWire(0);
 // TwoWire codecBus = TwoWire(1);
+TwoWire                       myWire = TwoWire(0);                      // universal I2C interface
 
-
-/*
-AudioInfo                     audio_info(44200, 2, 16);                // sampling rate, # channels, bit depth
-SineWaveGenerator<int16_t>    sine_wave(32000);                        // amplitude
-GeneratedSoundStream<int16_t> sound_stream(sine_wave);                 // sound generator
-DriverPins                    my_pins;                                 // board pins
-AudioBoard                    audio_board(AudioDriverES8388, my_pins); // audio board
-I2SCodecStream                i2s_stream(audio_board);             // i2s codec
-StreamCopy                    copier(i2s_stream, sound_stream);    // stream copy sound generator to i2s codec
-TwoWire                       myWire = TwoWire(0);                     // universal I2C interface
-*/
-
-
-AudioInfo                     audio_info(44200, 2, 16);                 // sampling rate, # channels, bit depth
+AudioInfo                     info_stereo(44200, 2, 16);                // sampling rate, # channels, bit depth
+AudioInfo                     info_mono(44200, 1, 16);                  // sampling rate, # channels, bit depth
 DriverPins                    my_pins;                                  // board pins
 AudioBoard                    audio_board(AudioDriverES8388, my_pins);  // audio board
 I2SCodecStream                i2s_stream(audio_board);                  // i2s codec
-VolumeStream                  outVol(i2s_stream);                       // vol outputs to i2s_stream (output)
-VolumeStream                  inVol(i2s_stream);                        // vol acceps from i2s_stream
-StreamCopy                    copier(outVol, inVol);                    // copy i2s_stream (input) to inVol (output)
-TwoWire                       myWire = TwoWire(0);                      // universal I2C interface
 
+
+SineWaveGenerator<int16_t>    sine_wave(32000);
+GeneratedSoundStream<int16_t> sound_stream(sine_wave);
+
+SineWaveGenerator<int16_t>    sine_wave2(32000);
+GeneratedSoundStream<int16_t> sound_stream2(sine_wave2);
+
+// ChannelSplitOutput            input_split;                              // splits the stereo input stream into two mono streams
+// ChannelsSelectOutput          input_split;
+VolumeStream                  in_vol;                                   // input volume control
+VolumeStream                  out_vol;                                  // output volume control
+// FilteredStream<int16_t, float> filtered(inVol, audio_info.channels);
+InputMerge<int16_t>           output_merge;                             // merges two mono channels into a stereo output
+// StreamCopy                    copier(input_split, i2s_stream);          // copier(output, input) drives data from stream to the splitter
+StreamCopy copier(i2s_stream, output_merge);
 
 // example of i2s codec for both input and output: https://github.com/pschatzmann/arduino-audio-tools/blob/main/examples/examples-audiokit/streams-audiokit-filter-audiokit/streams-audiokit-filter-audiokit.ino
 
 
+int t = 0;
+int counter = 0;
+
+float coef[] = { 0.021, 0.096, 0.146, 0.096, 0.021};
 
 void setup() {
   // put your setup code here, to run once:
@@ -55,6 +59,60 @@ void setup() {
   LOGLEVEL_AUDIODRIVER = AudioDriverDebug;
   // LOGLEVEL_AUDIODRIVER = AudioDriverInfo;
 
+
+  // filtered.setFilter(0, new FIR<float>(coef));
+  // filtered.setFilter(1, new FIR<float>(coef));
+
+  
+
+  my_pins.addI2C(PinFunction::CODEC, CODEC_SCL, CODEC_SDA, CODEC_ADDR, CODEC_I2C_SPEED, myWire);
+  my_pins.addI2S(PinFunction::CODEC, CODEC_MCLK, CODEC_BCLK, CODEC_WS, CODEC_DO, CODEC_DI);
+  my_pins.begin();
+  audio_board.begin();
+
+
+  // Serial.println("Configure output device ..."); 
+  // // try changing the output channel with a CodecConfig
+  // CodecConfig cfg;
+  // // cfg.output_device = DAC_OUTPUT_LINE1;
+  // //cfg.input_device = ADC_INPUT_LINE1;
+  // cfg.output_device = DAC_OUTPUT_ALL;
+  // audio_board.setConfig(cfg);
+
+  Serial.println("I2S begin ..."); 
+  auto i2s_config = i2s_stream.defaultConfig(RXTX_MODE);
+  i2s_config.copyFrom(info_stereo);
+  // i2s_config.input_device = ADC_INPUT_LINE1;  // intend dto use RIN1, not sure 
+  // i2s_config.output_device  = DAC_OUTPUT_LINE1;
+  i2s_stream.begin(i2s_config); // this should apply I2C and I2S configuration
+
+/*
+  input_split.begin(info_stereo);
+  input_split.addOutput(in_vol, 0);
+  input_split.addOutput(dummy_1, 1);
+
+
+  input_split.addOutput(in_vol, 0);
+  input_split.begin(info_stereo);
+
+*/
+
+  sine_wave.begin(info_mono, N_B4); // 493.88 Hz
+  sine_wave2.begin(info_mono, N_C4);
+
+/*
+  in_vol.begin(info_mono);
+  in_vol.setVolume(1.0);
+  in_vol.setOutput(out_vol);
+
+  out_vol.begin(info_mono);
+  out_vol.setVolume(1.0);
+  out_vol.setOutput(output_merge);
+*/
+  
+  output_merge.add(sound_stream);
+  output_merge.add(sound_stream2);
+  output_merge.begin(info_stereo);
 
   // clockBus.begin(CLOCK_SDA, CLOCK_SCL, 100000);
   /*
@@ -77,82 +135,32 @@ void setup() {
   si5351.output_enable(SI5351_CLK1, 1);
   si5351.output_enable(SI5351_CLK2, 0);
   */
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { 
+  copier.copy();
 
-  /*
-  digitalWrite(LED_GRN, HIGH);
-  delay(1000);
-  digitalWrite(LED_GRN, LOW);
-  delay(1000);
-  */
-
-  my_pins.addI2C(PinFunction::CODEC, CODEC_SCL, CODEC_SDA, CODEC_ADDR, CODEC_I2C_SPEED, myWire);
-  my_pins.addI2S(PinFunction::CODEC, CODEC_MCLK, CODEC_BCLK, CODEC_WS, CODEC_DO, CODEC_DI);
-  my_pins.begin();
-  audio_board.begin();
-
-/*
-  Serial.println("Configure output device ..."); 
-  // try changing the output channel with a CodecConfig
-  CodecConfig cfg;
-  cfg.output_device = DAC_OUTPUT_LINE1;
-  //cfg.input_device = ADC_INPUT_LINE1;
-  // cfg.output_device = DAC_OUTPUT_ALL;
-  // audio_board.setConfig(cfg);
-  audio_board.begin(cfg);
-*/
-
-  Serial.println("I2S begin ..."); 
-  auto i2s_config = i2s_stream.defaultConfig(RXTX_MODE);
-  i2s_config.copyFrom(audio_info);
-  // i2s_config.input_device = ADC_INPUT_LINE1;  // intend dto use RIN1, not sure 
-  // i2s_config.output_device  = DAC_OUTPUT_LINE1;
-  i2s_stream.begin(i2s_config); // this should apply I2C and I2S configuration
-  
-
-  
-  inVol.begin(audio_info);
-  inVol.setVolume(1.0);
-  outVol.begin(audio_info);
-  outVol.setVolume(1.0);
-
-
-  // Setup sine wave
-  Serial.println("Sine wave begin...");
-  // sine_wave.begin(audio_info, N_B4); // 493.88 Hz
-
-  Serial.println("Setup completed ...");
-
-
-  int t = 0;
-  int counter = 0;
-  while(1) {
-    copier.copy();
-
-    if(millis() - t > 1000) {
-      AudioDriver *driver = audio_board.getDriver();
-      if(counter % 2 == 0) {
-        driver->setMute(false, 0);
-        driver->setMute(true, 1);
-        digitalWrite(LED_GRN, HIGH);
-        Serial.println("on");
-      }
-      else {
-        driver->setMute(true, 0);
-        driver->setMute(false, 1);
-        digitalWrite(LED_GRN, LOW);
-        Serial.println("off");
-      }      
-      counter++;
-      t = millis();
+  if(millis() - t > 1000) {
+    AudioDriver *driver = audio_board.getDriver();
+    if(counter % 2 == 0) {
+      driver->setMute(false, 0);
+      driver->setMute(true, 1);
+      digitalWrite(LED_GRN, HIGH);
+      Serial.println("on");
     }
-
-    if(millis() % 2 == 0)
-      digitalWrite(SPARE_0, HIGH);
-    else 
-      digitalWrite(SPARE_0, LOW);
+    else {
+      driver->setMute(true, 0);
+      driver->setMute(false, 1);
+      digitalWrite(LED_GRN, LOW);
+      Serial.println("off");
+    }      
+    counter++;
+    t = millis();
   }
+
+  if(millis() % 2 == 0)
+    digitalWrite(SPARE_0, HIGH);
+  else 
+    digitalWrite(SPARE_0, LOW);
 }
