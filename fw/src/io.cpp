19683a51a5388946773afbcafd8c0d0a5dd117fb
Max <ki6syd@gmail.com>
2024-09-19 00:03:33 -0700

    Fix USB connection / serial logic

--- fw/src/io.cpp
+++ fw/src/io.cpp
@@ -22,101 +22,123 @@ ICACHE_RAM_ATTR void buttonISR() {
   xSemaphoreGiveFromISR(btn_semaphore, NULL);
 }
 
+static void usbEventCallback(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+  if (event_base == ARDUINO_HW_CDC_EVENTS) {
+    switch (event_id) {
+      case ARDUINO_HW_CDC_CONNECTED_EVENT:
+        digitalWrite(LED_RED, HIGH);
+        break;
+    }
+  }
+}
+
+const char *_hwcdc_status[] = {
+  " USB Plugged but CDC is NOT connected\r\n",
+  " USB Plugged and CDC is connected\r\n",
+  " USB Unplugged and CDC NOT connected\r\n",
+  " USB Unplugged BUT CDC is connected :: PROBLEM\r\n",
+};
+
+const char *HWCDC_Status() {
+  int i = Serial.isPlugged() ? 0 : 2;
+  if (Serial.isConnected()) {
+    i += 1;
+  }
+  return _hwcdc_status[i];
+}
+
 
 void io_init() {
-    pinMode(LED_GRN, OUTPUT);
-    pinMode(LED_RED, OUTPUT);
-    pinMode(LED_DBG_0, OUTPUT);
-    pinMode(LED_DBG_1, OUTPUT);
-    pinMode(SPARE_0, OUTPUT);
+  pinMode(LED_GRN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_DBG_0, OUTPUT);
+  pinMode(LED_DBG_1, OUTPUT);
+  pinMode(SPARE_0, OUTPUT);
 
-    pinMode(BOOT_BTN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(BOOT_BTN), buttonISR, CHANGE);
-    pinMode(MIC_PTT, INPUT);
-    attachInterrupt(digitalPinToInterrupt(MIC_PTT), buttonISR, FALLING);
-    pinMode(KEY_DAH, INPUT);
-    attachInterrupt(digitalPinToInterrupt(KEY_DAH), buttonISR, FALLING);
-    pinMode(KEY_DIT, INPUT);
-    attachInterrupt(digitalPinToInterrupt(KEY_DIT), buttonISR, FALLING);
+  pinMode(BOOT_BTN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(BOOT_BTN), buttonISR, CHANGE);
+  pinMode(MIC_PTT, INPUT);
+  attachInterrupt(digitalPinToInterrupt(MIC_PTT), buttonISR, FALLING);
+  pinMode(KEY_DAH, INPUT);
+  attachInterrupt(digitalPinToInterrupt(KEY_DAH), buttonISR, FALLING);
+  pinMode(KEY_DIT, INPUT);
+  attachInterrupt(digitalPinToInterrupt(KEY_DIT), buttonISR, FALLING);
 
-    digitalWrite(SPARE_0, LOW);
-    digitalWrite(LED_DBG_0, LOW);
-    digitalWrite(LED_DBG_1, LOW);
+  digitalWrite(SPARE_0, LOW);
+  digitalWrite(LED_DBG_0, LOW);
+  digitalWrite(LED_DBG_1, LOW);
 
-    // delay gives time to see serial port messages on monitor
-    delay(5000);    
+  Serial.begin(DEBUG_SERIAL_SPEED);
+  // delay gives time to see serial port messages on monitor
+  delay(5000);
 
-    // only start the HWCDC serial port if something is connected
-    // fixes bad behavior where audio output from codec was corrupt if USB unplugged
-    if(Serial.isConnected()) {
-        Serial.begin(DEBUG_SERIAL_SPEED);
-        Serial.println("Serial port connected.");
-    } 
-     Serial.begin(DEBUG_SERIAL_SPEED);
+  // helps avoid some sort of overflow when serial is not connected
+  if(!Serial.isPlugged() && !Serial.isConnected())
+    Serial.end();
 
-    // PSRAM initialization
-    // https://community.platformio.org/t/how-to-use-psram-on-esp32-s3-devkitc-1-under-esp-idf/32127/17
-    if(psramInit())
-        Serial.println("\nPSRAM is correctly initialized");
-    else
-        Serial.println("PSRAM not available");
-    Serial.println(psramFound());
+  // PSRAM initialization
+  // https://community.platformio.org/t/how-to-use-psram-on-esp32-s3-devkitc-1-under-esp-idf/32127/17
+  if(psramInit())
+      Serial.println("\nPSRAM is correctly initialized");
+  else
+      Serial.println("PSRAM not available");
+  Serial.println(psramFound());
 
-    // check PSRAM
-    Serial.print("Size PSRAM: ");
-    Serial.println(ESP.getPsramSize());
-    Serial.print("Free PSRAM: ");
-    Serial.println(ESP.getFreePsram());
+  // check PSRAM
+  Serial.print("Size PSRAM: ");
+  Serial.println(ESP.getPsramSize());
+  Serial.print("Free PSRAM: ");
+  Serial.println(ESP.getFreePsram());
 
-    Serial.println("allocating to PSRAM...");
-    byte* psram_buffer = (byte*) ps_malloc(1024);
+  Serial.println("allocating to PSRAM...");
+  byte* psram_buffer = (byte*) ps_malloc(1024);
 
-    Serial.print("Free PSRAM: ");
-    Serial.println(ESP.getFreePsram());
+  Serial.print("Free PSRAM: ");
+  Serial.println(ESP.getFreePsram());
 
-    // run on core 0
-    xTaskCreatePinnedToCore(
-        spare_task_core_0,
-        "Debug LED spare time task",
-        4096,
-        NULL,
-        TASK_PRIORITY_LOWEST, // priority
-        &xSpareTaskHandle0,
-        0 // core
-    );
+  // run on core 0
+  xTaskCreatePinnedToCore(
+      spare_task_core_0,
+      "Debug LED spare time task",
+      4096,
+      NULL,
+      TASK_PRIORITY_LOWEST, // priority
+      &xSpareTaskHandle0,
+      0 // core
+  );
 
-    // run on core 1
-    xTaskCreatePinnedToCore(
-        spare_task_core_1,
-        "Debug LED spare time task",
-        4096,
-        NULL,
-        TASK_PRIORITY_LOWEST, // priority
-        &xSpareTaskHandle1,
-        1 // core
-    );
+  // run on core 1
+  xTaskCreatePinnedToCore(
+      spare_task_core_1,
+      "Debug LED spare time task",
+      4096,
+      NULL,
+      TASK_PRIORITY_LOWEST, // priority
+      &xSpareTaskHandle1,
+      1 // core
+  );
 
-    // run on core 0
-    xTaskCreatePinnedToCore(
-        blink_task,
-        "Blinky light",
-        4096,
-        NULL,
-        TASK_PRIORITY_LOWEST + 1, // priority
-        &xBlinkTaskHandle,
-        0 // core
-    );
+  // run on core 0
+  xTaskCreatePinnedToCore(
+      blink_task,
+      "Blinky light",
+      4096,
+      NULL,
+      TASK_PRIORITY_LOWEST + 1, // priority
+      &xBlinkTaskHandle,
+      0 // core
+  );
 
-    btn_semaphore = xSemaphoreCreateBinary();
-    xTaskCreatePinnedToCore(
-        tx_pulse_task,
-        "TX pulse generator",
-        4096,
-        NULL,
-        TASK_PRIORITY_HIGHEST, // priority
-        &xTxPulseTaskHandle,
-        1 // core
-    );
+  btn_semaphore = xSemaphoreCreateBinary();
+  xTaskCreatePinnedToCore(
+      tx_pulse_task,
+      "TX pulse generator",
+      4096,
+      NULL,
+      TASK_PRIORITY_HIGHEST, // priority
+      &xTxPulseTaskHandle,
+      1 // core
+  );
 }
 
 // if LED_DBG_x is *not* illuminated, spareTask is starved
